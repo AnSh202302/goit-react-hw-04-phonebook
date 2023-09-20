ba7af7e3d62c2c5ac5cb18b19199ed057343b38a
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-09-20 14:46:16 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,50 @@
-import { Component } from 'react';
-import { Form } from './Form/Form';
+import { useEffect, useState } from 'react';
+import Form from './Form/Form';
 import { nanoid } from 'nanoid';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export default function App() {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
 
-  createUser = data => {
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const createUser = data => {
     const contact = {
       ...data,
       id: nanoid(),
     };
-    if (this.state.contacts.find(el => el.name === data.name)) {
+    if (contacts.find(el => el.name === data.name)) {
       alert(data.name + ' is already in contacts.');
       return;
     }
-    this.setState(prev => ({
-      contacts: [...prev.contacts, contact],
-    }));
+    setContacts([...contacts, contact]);
   };
-
-  deleteUser = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(el => el.id !== id),
-    }));
-  };
-
-  handleSearch = e => {
-    this.setState(prev => ({
-      filter: e.target.value,
-    }));
-  };
-  getVisibleName = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleName = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(el =>
       el.name.toLowerCase().includes(filter.toLowerCase(normalizedFilter))
     );
   };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  const deleteUser = id => {
+    setContacts(contacts.filter(el => el.id !== id));
+  };
+  const handleSearch = e => {
+    setFilter(e.target.value);
+  };
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    const visibleName = this.getVisibleName();
-    return (
-      <>
-        <h1>Phonebook</h1>
-        <Form createUser={this.createUser} />
-        <h2>Contacts</h2>
-        <Filter filter={this.state.filter} handleSearch={this.handleSearch} />
-        <ContactList contacts={visibleName} deleteUser={this.deleteUser} />
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <Form createUser={createUser} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} handleSearch={handleSearch} />
+      <ContactList contacts={getVisibleName()} deleteUser={deleteUser} />
+    </>
+  );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,53 +1,55 @@
-// import { nanoid } from 'nanoid';
-import { Component } from 'react';
-const INITIAL_STATE = {
-  name: '',
-  number: '',
-};
+import { useState } from 'react';
 
-export class Form extends Component {
-  state = INITIAL_STATE;
-  handleGhange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+export default function Form({ createUser }) {
+  const [name, setName] = useState('');
+  const [number, setNamber] = useState('');
+
+  const handleGhange = e => {
+    switch (e.target.name) {
+      case 'name':
+        setName(e.target.value);
+        break;
+      case 'number':
+        setNamber(e.target.value);
+        break;
+      default:
+        return;
+    }
   };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
-    this.props.createUser(this.state);
-    this.setState(INITIAL_STATE);
+    createUser({ name, number });
+    setName('');
+    setNamber('');
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            value={this.state.name}
-            name="name"
-            pattern="[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleGhange}
-          />
-        </label>
-        <label>
-          Number
-          <input
-            type="tel"
-            value={this.state.number}
-            name="number"
-            pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange={this.handleGhange}
-          />
-        </label>
-        <button type="submit">Add contact</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          value={name}
+          name="name"
+          pattern="[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleGhange}
+        />
+      </label>
+      <label>
+        Number
+        <input
+          type="tel"
+          value={number}
+          name="number"
+          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleGhange}
+        />
+      </label>
+      <button type="submit">Add contact</button>
+    </form>
+  );
 }
